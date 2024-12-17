79adad74dea26dab41b20aa3c2d424d8e72bdba9
SumedhChobitkar <sumedhchobitkar04@gmail.com>
2024-12-17 11:48:09 +0530

    New Project Added

---
+++ src/test/java/com/OnlineQuizzApplication/OnlineQuizzApplication/OnlineQuizzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.OnlineQuizzApplication.OnlineQuizzApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineQuizzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
